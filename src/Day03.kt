54a001ed0c1f0145236d0e35aa22a3b79d72f2f0
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2021-12-03 07:04:09 -0500

    Solved Day 03 Part 1 and refactored to separate concerns

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,6 +1,29 @@
 fun main() {
+    fun inv(num: Int): Int =
+        num.toString(2)
+            .map { if (it == '1') '0' else '1' }
+            .joinToString("")
+            .toInt(2)
+
+    fun countBits(input: List<String>): IntArray {
+        val bitCounts = IntArray(input.first().length)
+        input.forEach {
+            it.forEachIndexed { i, ch ->
+                if (ch == '1') {
+                    bitCounts[i] += 1
+                }
+            }
+        }
+        return bitCounts
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val half = input.size / 2
+        val gamma = countBits(input).map {
+            if (it > half) '1' else '0'
+        }.joinToString("").toInt(2)
+
+        return gamma * inv(gamma)
     }
 
     fun part2(input: List<String>): Int {
@@ -8,10 +31,10 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("DayXX_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day03_test")
+    check(part1(testInput) == 198)
 
-    val input = readInput("DayXX")
+    val input = readInput("Day03")
     println(part1(input))
     println(part2(input))
 }
