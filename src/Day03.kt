555ba3ca149d199eb5529c35b80ce0031b928f57
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2021-12-03 16:03:03 -0500

    Solved Day 03 Part 2, refactored Part 1 solution

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,9 +1,8 @@
 fun main() {
-    fun inv(num: Int): Int =
-        num.toString(2)
-            .map { if (it == '1') '0' else '1' }
-            .joinToString("")
-            .toInt(2)
+
+    fun inv(num: Int) = num.toString(2)
+        .map { if (it == '1') '0' else '1' }
+        .joinToString("").toInt(2)
 
     fun countBits(input: List<String>): IntArray {
         val bitCounts = IntArray(input.first().length)
@@ -17,22 +16,41 @@ fun main() {
         return bitCounts
     }
 
-    fun part1(input: List<String>): Int {
+    fun gamma(input: List<String>): Int {
         val half = input.size / 2
-        val gamma = countBits(input).map {
+        return countBits(input).map {
             if (it > half) '1' else '0'
         }.joinToString("").toInt(2)
-
-        return gamma * inv(gamma)
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<String>): Int = gamma(input).let { it * inv(it) }
+
+    fun countBit(input: List<String>, place: Int): Int = input.count { it[place] == '1' }
+
+    fun bitCriteria(size: Int, count: Int, seq: CharSequence): Char =
+        if (count >= size - count) seq.first() else seq.last()
+
+    fun rating(input: List<String>, seq: CharSequence): Int {
+        var numbers = input
+        var i = 0
+        while (numbers.size > 1) {
+            val bitCriteria = bitCriteria(numbers.size, countBit(numbers, i), seq)
+            numbers = numbers.filter { it[i] == bitCriteria }
+            i += 1
+        }
+        return numbers[0].toInt(2) // .also { println(it) }
     }
+
+    fun o2Rating(input: List<String>) = rating(input, "10")
+
+    fun co2Rating(input: List<String>) = rating(input, "01")
+
+    fun part2(input: List<String>): Int = o2Rating(input) * co2Rating(input)
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day03_test")
     check(part1(testInput) == 198)
+    check(part2(testInput) == 230)
 
     val input = readInput("Day03")
     println(part1(input))
