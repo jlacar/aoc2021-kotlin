1e779365ae145b8873e9a6c121a843ce4c891e1c
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2021-12-03 19:23:35 -0500

    Solved AoC 2015 Day 01 Part 2

--- src/aoc2015/Day01.kt
+++ src/aoc2015/Day01.kt
@@ -11,7 +11,18 @@ fun main() {
     }
 
     fun part2(input: String): Int {
-        return input.length
+        var floor = 0
+        var pos = 0
+        val hasNotMovedIntoBasement: (ch: Char) -> Boolean = {
+            when (it) {
+                '(' -> floor += 1
+                ')' -> floor -= 1
+            }
+            pos += 1
+            floor != -1
+        }
+        input.takeWhile { hasNotMovedIntoBasement(it) }
+        return pos
     }
 
     fun part1Test(data: List<String>, expected: Int): Unit =
@@ -21,6 +32,9 @@ fun main() {
     part1Test(listOf("(())", "()()"), 0)
     part1Test(listOf("(((", "(()(()(", "))((((("), 3)
     part1Test(listOf(")))", ")())())"), -3)
+
+    check(part2(")") == 1)
+    check(part2("()())") == 5)
 
     val input = readInput("aoc2015/Day01")
     println(part1(input.first()))
