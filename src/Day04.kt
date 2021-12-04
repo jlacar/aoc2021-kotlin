f35957fd949a2b49febf623bf9a7d8aa9c7379e2
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2021-12-04 06:02:25 -0500

    Slice and dice input for Part 1

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,6 +1,16 @@
+/**
+ * Giant Squid Bingo
+ *
+ * 5x5 grids, only complete rows and columns win, no diagonals.
+ */
 fun main() {
+    /**
+     * Final score is sum of unmarked numbers * winning number called
+     */
     fun part1(input: List<String>): Int {
-        return input.size
+        println(input.first())
+        println(input.slice(2..input.lastIndex).chunked(6))
+        return 0
     }
 
     fun part2(input: List<String>): Int {
@@ -9,9 +19,13 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day04_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 4512)
 
     val input = readInput("Day04")
     println(part1(input))
     println(part2(input))
+}
+
+class Board {
+    val numbers: Set<Array<Int>> = mutableSetOf()
 }
