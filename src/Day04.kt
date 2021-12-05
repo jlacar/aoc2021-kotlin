c1b58168247c6f3da804a90ee420c9efb9c6b953
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2021-12-05 07:39:59 -0500

    Day 04 git er done - Part 1

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,31 +1,100 @@
+import java.util.*
+
 /**
  * Giant Squid Bingo
  *
  * 5x5 grids, only complete rows and columns win, no diagonals.
  */
 fun main() {
+    fun numberPicker(input: List<String>) = input.first().split(",").map { it.toInt() }
+
+    fun boardsFrom(input: List<String>) =
+        input.slice(2..input.lastIndex).chunked(6).map { Board(it.take(5)) }
+
+    fun noBingo(boards: List<Board>, pick: Int): Boolean {
+        for (board in boards) {
+            if (board.mark(pick).isBingo) return false
+        }
+        return true
+    }
+
     /**
      * Final score is sum of unmarked numbers * winning number called
      */
     fun part1(input: List<String>): Int {
-        println(input.first())
-        println(input.slice(2..input.lastIndex).chunked(6))
-        return 0
+        val boards: List<Board> = boardsFrom(input)
+        numberPicker(input).takeWhile { pick -> noBingo(boards, pick) }
+        return boards.first { it.isBingo }.score
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
+    val testBoard = Board(listOf("1 2 3", "4 5 6", "7 8 9"))
+    check(!testBoard.isBingo && testBoard.score == 45)
+
+    testBoard.mark(11)
+    check(!testBoard.isBingo && testBoard.score == 45)
+
+    testBoard.mark(4)
+    check(!testBoard.isBingo)
+
+    testBoard.mark(5)
+    check(!testBoard.isBingo)
+
+    testBoard.mark(6)
+    check(testBoard.isBingo)
+
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day04_test")
     check(part1(testInput) == 4512)
 
     val input = readInput("Day04")
-    println(part1(input))
-    println(part2(input))
+    println(part1(input)) // 63552
+    // println(part2(input))
 }
 
-class Board {
-    val numbers: Set<Array<Int>> = mutableSetOf()
+fun toIntArray(s: String): IntArray = s.trim()
+    .split(Regex("""\s+"""))
+    .map { it.toInt() }.toIntArray()
+
+class Board(lines: List<String>) {
+    private val markedPicks: MutableList<Int> = mutableListOf()
+    private val numbers: List<IntArray> = lines.map { toIntArray(it) }
+
+    private var _score: Int = numbers.sumOf { it.sum() }
+    val score: Int get() = _score * (markedPicks.lastOrNull() ?: 1)
+
+    private var _isBingo: Boolean = false
+    val isBingo: Boolean get() = _isBingo
+
+    private fun isFound(pick: Int) = numbers.any { it.contains(pick) }
+
+    private fun allPicked(line: List<Int>) = markedPicks.containsAll(line)
+
+    private fun colNums(col: Int): List<Int> = numbers.map { it[col] }
+
+    private fun checkBingo(pick: Int) {
+        numbers.forEachIndexed { row, rowNums ->
+            if (rowNums.contains(pick)) {
+                val col = rowNums.indexOf(pick)
+                _isBingo = allPicked(rowNums.toList()) || allPicked(colNums(col))
+            }
+        }
+    }
+
+    fun mark(pick: Int): Board {
+        if (isFound(pick)) {
+            markedPicks.add(pick).also {
+                _score -= pick
+                checkBingo(pick)
+            }
+        }
+        return this
+    }
 }
+
+//    override fun toString(): String =
+//        numbers.map { it.joinToString(separator = ",") }
+//            .joinToString(",\n")
