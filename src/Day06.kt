f2b5f31ac57433bdbab6db0dc3913f18f823e439
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2021-12-06 18:23:40 -0500

    Day 06 simplified array initialization. Used Long literal 1L

--- src/Day06.kt
+++ src/Day06.kt
@@ -4,7 +4,7 @@
  * https://adventofcode.com/2021/day/6
  */
 fun main() {
-    fun naiveSolution(input: List<Int>, days: Int = 80): Long {
+    fun naiveSolution(input: List<Int>, days: Int): Long {
         var school = mutableListOf<Int>().also { it.addAll(input) }
         repeat(days) {
             val newFish = school.count { it == 0 }
@@ -20,9 +20,9 @@ fun main() {
         return school.size.toLong()
     }
 
-    fun moreEfficientSolution(input: List<Int>, days: Int = 80): Long {
+    fun moreEfficientSolution(input: List<Int>, days: Int): Long {
         val timers = LongArray(9) { 0L }
-        input.forEach { timers.set(it, timers.get(it) + 1) }
+        input.forEach { timers[it] += 1L }
         repeat(days) {
             val newFish = timers[0]
             for (i in 0..7) {
@@ -35,17 +35,17 @@ fun main() {
     }
 
     fun part1(input: List<Int>): Long =
+        moreEfficientSolution(input, 80)
 //        naiveSolution(input)
-        moreEfficientSolution(input)
 
     fun part2(input: List<Int>): Long = moreEfficientSolution(input, 256)
 
     // test if implementation meets criteria from the description, like:
     val testInput = toIntList(readInput("Day06_test").first())
     check(part1(testInput) == 5934L)
-    check(part2(testInput) == 26984457539)
+    check(part2(testInput) == 26984457539L)
 
     val input = toIntList(readInput("Day06").first())
     println(part1(input).also { check(it == 345793L) }) // solved
-    println(part2(input).also { check(it == 1572643095893L)}) // solved
+    println(part2(input).also { check(it == 1572643095893L) }) // solved
 }
