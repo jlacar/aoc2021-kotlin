da5eaa1261fc457b68e45b5ec8b426652c886172
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2021-12-06 16:42:34 -0500

    Refactored Day 06: used repeat() instead

--- src/Day06.kt
+++ src/Day06.kt
@@ -6,7 +6,7 @@
 fun main() {
     fun bruteForcePart1(input: List<Int>, days: Int): Long {
         var school = mutableListOf<Int>().also { it.addAll(input) }
-        for (day in 1..days) {
+        repeat(days) {
             val newFish = school.count { it == 0 }
             school.replaceAll { timer ->
                 when (timer) {
@@ -20,9 +20,16 @@ fun main() {
         return school.size.toLong()
     }
 
-    fun part1(input: List<Int>, days: Int = 80): Long {
-        return bruteForcePart1(input, days)
+    fun differentWayPart1(input: List<Int>): Long {
+        val counts = LongArray(9) { 1L }
+        // println("""${counts.joinToString(",", "[", "]")} == ${counts.sum()}""")
+        for (day in 1..80) {}
+        return counts.sum()
     }
+
+    fun part1(input: List<Int>, days: Int = 80): Long =
+        bruteForcePart1(input, days)
+//        differentWayPart1(input)
 
     fun part2(input: List<Int>): Long {
         return part1(input, 180)
@@ -31,7 +38,7 @@ fun main() {
     // test if implementation meets criteria from the description, like:
     val testInput = toIntList(readInput("Day06_test").first())
     check(part1(testInput) == 5934L)
-    check(part2(testInput) == 26984457539)
+//    check(part2(testInput) == 26984457539)
 
     val input = toIntList(readInput("Day06").first())
     println(part1(input).also { check(it == 345793L) })
