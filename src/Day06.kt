619fa66fea699d612bfbca097c8f8dfe9ccf28fe
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2021-12-06 16:16:22 -0500

    Refactored Day 06: extracted original brute force to make room for different way

--- src/Day06.kt
+++ src/Day06.kt
@@ -4,29 +4,36 @@
  * https://adventofcode.com/2021/day/6
  */
 fun main() {
-    fun part1(input: List<Int>): Int {
+    fun bruteForcePart1(input: List<Int>, days: Int): Long {
         var school = mutableListOf<Int>().also { it.addAll(input) }
-        for (day in 1..80) {
+        for (day in 1..days) {
             val newFish = school.count { it == 0 }
-            school.replaceAll { timer -> when(timer) {
-                in 1..8 -> timer - 1
-                0 -> 6
-                else -> timer
-            } }
-            school.addAll(List(newFish) {8})
+            school.replaceAll { timer ->
+                when (timer) {
+                    in 1..8 -> timer - 1
+                    0 -> 6
+                    else -> timer
+                }
+            }
+            school.addAll(List(newFish) { 8 })
         }
-        return school.size
+        return school.size.toLong()
     }
 
-    fun part2(input: List<Int>): Int {
-        return input.size
+    fun part1(input: List<Int>, days: Int = 80): Long {
+        return bruteForcePart1(input, days)
+    }
+
+    fun part2(input: List<Int>): Long {
+        return part1(input, 180)
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = toIntList(readInput("Day06_test").first())
-    check(part1(testInput) == 5934)
+    check(part1(testInput) == 5934L)
+    check(part2(testInput) == 26984457539)
 
     val input = toIntList(readInput("Day06").first())
-    println(part1(input).also { check(it == 345793) })
-    println(part2(input))
+    println(part1(input).also { check(it == 345793L) })
+//    println(part2(input))
 }
