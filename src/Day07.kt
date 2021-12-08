a67a201675cd2b7214137938cbf3aeac9f3ea021
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2021-12-08 13:52:24 -0500

    Refactoring Day 07: removing older arrangement

--- src/Day07.kt
+++ src/Day07.kt
@@ -12,9 +12,6 @@ fun main() {
 
     fun compounding(n: Int) = n * (n + 1) / 2
 
-    fun costToAlignAll(crabs: Map<Int, Int>, position: Int, fuelUsed: (Int) -> Int): Int =
-        crabs.keys.filter { it != position }.sumOf(fuelUsed)
-
     fun costToAlignAll2(crabs: Map<Int, Int>, position: Int, fuelUsed: (Int, Int) -> Int): Int =
         crabs.keys.filter { it != position }.sumOf { fuelUsed(it, position) * crabs[it]!! }
 
@@ -35,20 +32,12 @@ fun main() {
     fun part2(crabs: Map<Int, Int>): Int = cheapestAlignmentOf(crabs) { position ->
         costToAlignAll2(crabs, position, compoundCostFormula) }
 
-    fun linearCost(crabs: Map<Int, Int>): Int = cheapestAlignmentOf(crabs)
-        { position -> costToAlignAll(crabs, position) { fuelToMove(it, position) * crabs[it]!! } }
-
-    fun compoundCost(crabs: Map<Int, Int>): Int = cheapestAlignmentOf(crabs)
-        { position -> costToAlignAll(crabs, position) { compounding(fuelToMove(it, position)) * crabs[it]!! } }
-
     // test if implementation meets criteria from the description, like:
     val testInput = toIntList("16,1,2,0,4,2,7,1,2,14").groupingBy { it }.eachCount()
-    check(linearCost(testInput) == 37)
-    check(compoundCost(testInput) == 168)
+    check(part1(testInput) == 37)
+    check(part2(testInput) == 168)
 
     val crabs = toIntList(readInput("Day07").first()).groupingBy { it }.eachCount()
-    println(linearCost(crabs).also { check(it == 336701) })         // accepted solution, Part 1
     println(part1(crabs).also { check(it == 336701) })         // accepted solution, Part 1
-    println(compoundCost(crabs).also { check(it == 95167302) })   // accepted solution, Part 2
     println(part2(crabs).also { check(it == 95167302) })   // accepted solution, Part 2
 }
