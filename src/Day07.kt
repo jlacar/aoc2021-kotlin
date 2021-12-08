e774433c14f231b1aa5486b67888fc44702a7f3b
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2021-12-08 13:47:06 -0500

    Refactoring Day 07: trying alternate arrangement

--- src/Day07.kt
+++ src/Day07.kt
@@ -15,12 +15,23 @@ fun main() {
     fun costToAlignAll(crabs: Map<Int, Int>, position: Int, fuelUsed: (Int) -> Int): Int =
         crabs.keys.filter { it != position }.sumOf(fuelUsed)
 
+    fun costToAlignAll2(crabs: Map<Int, Int>, position: Int, fuelUsed: (Int, Int) -> Int): Int =
+        crabs.keys.filter { it != position }.sumOf { fuelUsed(it, position) * crabs[it]!! }
+
     fun allPossiblePositionsOf(crabs: Map<Int, Int>) = (crabs.minOf { it.key }..crabs.maxOf { it.key })
 
     fun cheapestAlignmentOf(crabs: Map<Int, Int>, costToAlign: (Int) -> Int): Int =
         allPossiblePositionsOf(crabs).minOf(costToAlign)
 
     fun fuelToMove(crab: Int, toPosition: Int) = abs(crab - toPosition)
+
+    val linearCostFormula: (Int, Int) -> Int = { crab: Int, position: Int -> fuelToMove(crab, position) }
+
+    val compoundCostFormula: (Int, Int, Int) -> Int = { crab: Int, position: Int, count: Int ->
+        compounding(fuelToMove(crab, position)) * count }
+
+    fun part1(crabs: Map<Int, Int>): Int = cheapestAlignmentOf(crabs) { position ->
+        costToAlignAll2(crabs, position, linearCostFormula) }
 
     fun linearCost(crabs: Map<Int, Int>): Int = cheapestAlignmentOf(crabs)
         { position -> costToAlignAll(crabs, position) { fuelToMove(it, position) * crabs[it]!! } }
@@ -35,5 +46,6 @@ fun main() {
 
     val crabs = toIntList(readInput("Day07").first()).groupingBy { it }.eachCount()
     println(linearCost(crabs).also { check(it == 336701) })         // accepted solution, Part 1
+    println(part1(crabs).also { check(it == 336701) })         // accepted solution, Part 1
     println(compoundCost(crabs).also { check(it == 95167302) })   // accepted solution, Part 2
 }
