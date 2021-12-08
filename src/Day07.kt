e87e0a4b9bc3b607973c16592d62818e7a8e500d
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2021-12-08 07:00:35 -0500

    Day 07 DRY-ed out

--- src/Day07.kt
+++ src/Day07.kt
@@ -19,24 +19,20 @@ fun main() {
         nodes.keys.filter { it != source }
             .sumOf { gaussSum(abs(it - source)) * nodes[it]!! }
 
-    fun part1(input: List<Int>): Int {
-        val nodes = input.groupingBy { it }.eachCount()
-        return (nodes.minOf { it.key } .. nodes.maxOf { it.key })
+    fun part1(nodes: Map<Int, Int>): Int =
+        (nodes.minOf { it.key } .. nodes.maxOf { it.key })
             .minOf { source -> distancesFrom(source, nodes) }
-    }
 
-    fun part2(input: List<Int>): Int {
-        val nodes = input.groupingBy { it }.eachCount()
-        return (nodes.minOf { it.key } .. nodes.maxOf { it.key })
+    fun part2(nodes: Map<Int, Int>): Int =
+        (nodes.minOf { it.key } .. nodes.maxOf { it.key })
             .minOf { source -> gaussDistancesFrom(source, nodes) }
-    }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = toIntList("16,1,2,0,4,2,7,1,2,14")
+    val testInput = toIntList("16,1,2,0,4,2,7,1,2,14").groupingBy { it }.eachCount()
     check(part1(testInput) == 37)
     check(part2(testInput) == 168)
 
-    val input = toIntList(readInput("Day07").first())
-    println(part1(input).also { check(it == 336701) }) // verified solution
-    println(part2(input).also { check(it == 95167302) }) // verified solution
+    val nodes = toIntList(readInput("Day07").first()).groupingBy { it }.eachCount()
+    println(part1(nodes).also { check(it == 336701) }) // verified solution
+    println(part2(nodes).also { check(it == 95167302) }) // verified solution
 }
