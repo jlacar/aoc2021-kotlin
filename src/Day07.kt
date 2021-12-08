2d2ff826ec3df419197b89aa9072556204d286fb
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2021-12-08 05:14:51 -0500

    Day 07 Part 1 Git Er Done

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,3 +1,5 @@
+import kotlin.math.abs
+
 /**
  * AoC 2021, Day 07: The Treachery of Whales
  * https://adventofcode.com/2021/day/7
@@ -5,8 +7,22 @@
  * Align the crabs and escape the giant whale
  */
 fun main() {
+    /**
+     * Dijkstra's shortest path from a single source node
+     */
     fun part1(input: List<Int>): Int {
-        return input.size
+        val frequencies = input.groupingBy { it }.eachCount().also { println("frequencies (${it.size}): $it") }
+        var shortest = Int.MAX_VALUE
+        for (source in frequencies.keys) {
+            val others = frequencies.keys.filter { it != source }
+//                    .also { println("source: $source, others: ${it}") }
+
+            val distance = others.map { abs(it - source) * frequencies[it]!! }.sum()
+//                    .also { println("distance: $it") }
+
+            if (distance < shortest) shortest = distance
+        }
+        return shortest
     }
 
     fun part2(input: List<Int>): Int {
@@ -18,6 +34,6 @@ fun main() {
     check(part1(testInput) == 37)
 
     val input = toIntList(readInput("Day07").first())
-    println(part1(input))
-    println(part2(input))
+    println(part1(input).also { check(it == 336701) }) // solved
+//    println(part2(input))
 }
