3711c2ff9d56b430233f54cfb92421bb42a5d994
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2021-12-08 06:52:39 -0500

    Day 07 both parts solved and refactored

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,25 +1,34 @@
 import kotlin.math.abs
-import kotlin.math.min
 
 /**
  * AoC 2021, Day 07: The Treachery of Whales
  * https://adventofcode.com/2021/day/7
  *
  * Align the crabs and escape the giant whale
+ *
+ * See also: https://coderanch.com/t/747834/AoC-Day-Solutions-Spoilers
  */
 fun main() {
     fun distancesFrom(source: Int, nodes: Map<Int, Int>) =
         nodes.keys.filter { it != source }
-            .map { abs(it - source) * nodes[it]!! }
-            .sum()
+            .sumOf { abs(it - source) * nodes[it]!! }
+
+    fun gaussSum(n: Int) = n * (n + 1) / 2
+
+    fun gaussDistancesFrom(source: Int, nodes: Map<Int, Int>) =
+        nodes.keys.filter { it != source }
+            .sumOf { gaussSum(abs(it - source)) * nodes[it]!! }
 
     fun part1(input: List<Int>): Int {
         val nodes = input.groupingBy { it }.eachCount()
-        return nodes.keys.minOf { source -> distancesFrom(source, nodes) }
+        return (nodes.minOf { it.key } .. nodes.maxOf { it.key })
+            .minOf { source -> distancesFrom(source, nodes) }
     }
 
     fun part2(input: List<Int>): Int {
-        return input.size
+        val nodes = input.groupingBy { it }.eachCount()
+        return (nodes.minOf { it.key } .. nodes.maxOf { it.key })
+            .minOf { source -> gaussDistancesFrom(source, nodes) }
     }
 
     // test if implementation meets criteria from the description, like:
@@ -28,6 +37,6 @@ fun main() {
     check(part2(testInput) == 168)
 
     val input = toIntList(readInput("Day07").first())
-    println(part1(input).also { check(it == 336701) }) // solved
-    println(part2(input))
+    println(part1(input).also { check(it == 336701) }) // verified solution
+    println(part2(input).also { check(it == 95167302) }) // verified solution
 }
