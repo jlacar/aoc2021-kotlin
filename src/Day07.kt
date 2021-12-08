29184dc481f0632881114da213c663ddf5820af9
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2021-12-08 08:17:46 -0500

    Refactored Day 07 to use names from problem description

--- src/Day07.kt
+++ src/Day07.kt
@@ -12,24 +12,24 @@ fun main() {
 
     fun gaussSum(n: Int) = n * (n + 1) / 2
 
-    fun distanceTo(source: Int, nodes: Map<Int, Int>, calc: (Int) -> Int): Int =
-        nodes.keys.filter { it != source }.sumOf(calc)
+    fun distanceTo(position: Int, crabs: Map<Int, Int>, fuelUsed: (Int) -> Int): Int =
+        crabs.keys.filter { it != position }.sumOf(fuelUsed)
 
     fun cheapestAlignmentOf(nodes: Map<Int, Int>, fuelUsed: (Int) -> Int): Int =
         (nodes.minOf { it.key } .. nodes.maxOf { it.key }).minOf(fuelUsed)
 
-    fun part1(nodes: Map<Int, Int>): Int = cheapestAlignmentOf(nodes)
-        { source -> distanceTo(source, nodes) { abs(it - source) * nodes[it]!! } }
+    fun part1(crabs: Map<Int, Int>): Int = cheapestAlignmentOf(crabs)
+        { position -> distanceTo(position, crabs) { abs(it - position) * crabs[it]!! } }
 
-    fun part2(nodes: Map<Int, Int>): Int = cheapestAlignmentOf(nodes)
-        { source -> distanceTo(source, nodes) { gaussSum(abs(it - source)) * nodes[it]!! } }
+    fun part2(crabs: Map<Int, Int>): Int = cheapestAlignmentOf(crabs)
+        { position -> distanceTo(position, crabs) { gaussSum(abs(it - position)) * crabs[it]!! } }
 
     // test if implementation meets criteria from the description, like:
     val testInput = toIntList("16,1,2,0,4,2,7,1,2,14").groupingBy { it }.eachCount()
     check(part1(testInput) == 37)
     check(part2(testInput) == 168)
 
-    val nodes = toIntList(readInput("Day07").first()).groupingBy { it }.eachCount()
-    println(part1(nodes).also { check(it == 336701) }) // verified solution
-    println(part2(nodes).also { check(it == 95167302) }) // verified solution
+    val crabs = toIntList(readInput("Day07").first()).groupingBy { it }.eachCount()
+    println(part1(crabs).also { check(it == 336701) }) // verified solution
+    println(part2(crabs).also { check(it == 95167302) }) // verified solution
 }
